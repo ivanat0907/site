731ac926ddb1af2383e3ba0f55b2fdd781d1ac29
Ivana Topalovic <ivanat0907@gmail.com>
2023-01-28 20:28:55 +0100

    aa

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-
+/*aaaa*/
   constructor(private router: Router) {
     
   }
